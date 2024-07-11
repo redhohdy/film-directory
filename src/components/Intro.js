b2e1cecdcea16cee170285e4867aaa6856662613
redhohdy <redhohidayatullah@gmail.com>
2024-07-11 17:30:24 +0700

    Initial commit

---
+++ src/components/Intro.js
@@ -0,0 +1,24 @@
+import { Col, Container, Row, Button } from "react-bootstrap"
+
+const Intro = () => {
+  return (
+    <div className="intro">
+      <Container className="text-white text-center d-flex justify-content-center align-items-center">
+        <Row>
+          <Col>
+            <div className="title">NONTON GRATIS</div>
+            <div className="title">GAK PAKE KARCIS</div>
+            <div className="introButton mt-4 text-center">
+              <Button variant="dark">Lihat Semua List</Button>
+            </div>
+            <div className="introButton mt-4 text-center">
+              <Button href="https://opaku1.000webhostapp.com/" variant="dark">OPAKU</Button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  )
+}
+
+export default Intro
